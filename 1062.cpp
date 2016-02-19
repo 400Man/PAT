87d5d2bd2ed3bb95cbb04ac15665c2461e9be1d0
Bisgates <bisgates@outlook.com>
2016-02-19 21:52:57 +0800

    说白了，小心点就好了

--- 1062.cpp
+++ 1062.cpp
@@ -1,5 +1,3 @@
-//#  talent idea
-
 #include<stdio.h>
 #include<algorithm>
 #include<string.h>
@@ -12,52 +10,46 @@ struct T{
   int virtue;
   int tag;
   int total;
-  int foo;
 };
 
 bool cmp(T a,T b){
-  if(a.foo != b.foo)
-    return a.foo < b.foo;
   if(a.tag != b.tag)
     return a.tag < b.tag;
   if(a.total != b.total)
     return a.total > b.total;
   if(a.virtue != b.virtue)
     return a.virtue > b.virtue;
-  int u = strcmp(a.name,b.name);
-    return u < 0;
+  return strcmp(a.name,b.name) < 0;
 }
 
 int main()
 {
-  int n,score1,score2,m = 0;
+  int n,score1,score2;
   scanf("%d %d %d",&n,&score1,&score2);
+  int m = n;
   T person[n];
   for(int i = 0; i < n; i++){
     scanf("%s %d %d",person[i].name,&person[i].virtue,&person[i].talent);
+    person[i].total = person[i].talent + person[i].virtue;
     if(person[i].virtue < score1 || person[i].talent < score1)
     {
-      person[i].foo = 1;
-      m++;
+      person[i].tag = 5;
+      m--;
     }
-    else
-      person[i].foo = 0;
-      
-    if(person[i].talent >= score2 && person[i].virtue >= score2)
+    else if(person[i].talent >= score2 && person[i].virtue >= score2)
       person[i].tag = 1;
-    if(person[i].talent < score2 && person[i].virtue >= score2)
+    else if(person[i].talent < score2 && person[i].virtue >= score2)
       person[i].tag = 2;
-    if(person[i].talent < score2 && person[i].virtue < score2)
+    else if(person[i].virtue >= person[i].talent)
       person[i].tag = 3;
-    if(person[i].talent >= score2 && person[i].virtue < score2)
+    else
       person[i].tag = 4;
-      
-    person[i].total = person[i].talent + person[i].virtue;
   }
+  
   sort(person,person+n,cmp);
   
-  printf("%d\n",n-m);
-  for(int i = 0; i < n-m; i++)
+  printf("%d\n",m);
+  for(int i = 0; i < m; i++)
     printf("%s %d %d\n",person[i].name,person[i].virtue,person[i].talent);
   return 0;
 }
